1a28cfdf3652049eccb2df79fc46aaba30a5286f
joel <joelswaku@gmail.com>
2025-07-11 21:50:50 -0600

    latest

--- backend/routes/notesRoutes.js
+++ backend/routes/notesRoutes.js
@@ -34,28 +34,20 @@ import {
   getSingleNote,
   updateNote,
   deleteNote,
-  //searchNotes,
 } from "../controllers/notes.controller.js";
 import { protect } from "../middleware/protect.js";
-//import { limiter } from "../middleware/rateLimiter.js";
-
 
 const router = express.Router();
 
 router.use(protect); // protect all routes below
 
-
 router.post("/", createNote);
-router.get("/",  getAllNotes);
+router.get("/", getAllNotes);
 router.get("/:id", getSingleNote);
 router.put("/:id", updateNote);
-router.delete("/:id" , deleteNote)
+router.delete("/:id", deleteNote);
 
-// If you want a search route, uncomment and ensure its path doesn't conflict
-// e.g., router.get("/search", searchNotes);
-
-// Remove this duplicate line:
-// router.get("/:id", getSingleNote); // THIS IS THE PROBLEM!
-
+// If you want a search route, add it BEFORE `/:id`:
+// router.get("/search", searchNotes);
 
 export default router;
