1635168f79ba45759c0f65ecc257f7fc745a9e2c
joel <joelswaku@gmail.com>
2025-07-11 22:01:41 -0600

    latest one

--- backend/routes/notesRoutes.js
+++ backend/routes/notesRoutes.js
@@ -1,32 +1,3 @@
-// import express from "express";
-// import {
-//   createNote,
-//   getAllNotes,
-//   getSingleNote,
-//   updateNote,
-//   deleteNote,
-//   searchNotes,
-// } from "../controllers/notes.controller.js";
-// import { protect } from "../middleware/protect.js";
-// //import { limiter } from "../middleware/rateLimiter.js";
-
-
-// const router = express.Router();
-
-// router.use(protect); // protect all routes below
-
-
-// router.post("/", createNote);
-// router.get("/",  getAllNotes);
-// router.get("/:id", getSingleNote);
-// router.put("/:id", updateNote);
-// router.delete("/:id" , deleteNote)
-
-// router.get("/search", searchNotes);
-
-
-
-// export default router;
 import express from "express";
 import {
   createNote,
