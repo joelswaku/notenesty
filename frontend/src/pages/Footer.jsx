45cebd6f805e5b297ea573c7d078a38a754afdfa
joel <joelswaku@gmail.com>
2025-07-13 20:49:31 -0600

    footer

--- frontend/src/pages/Footer.jsx
+++ frontend/src/pages/Footer.jsx
@@ -3,7 +3,8 @@ import { Link } from 'react-router-dom';
 
 function Footer() {
   return (
-    <footer className="mt-12 bg-white dark:bg-gray-900 border-t border-gray-200 dark:border-gray-800 py-6 text-gray-700 dark:text-gray-300 ">
+     <footer className="w-full mt-12 bg-white dark:bg-gray-900 border-t border-gray-200 dark:border-gray-800 py-6 text-gray-700 dark:text-gray-300">
+
       <div className="max-w-5xl mx-auto px-4 flex flex-col md:flex-row justify-between items-center gap-4">
         <div className="text-sm">
           &copy; {new Date().getFullYear()} Notesty. All rights reserved.
